2f9c79efdbb96e69be8b8877687ce3a3760ca4f6
jpclrocha <jpclrocha@gmail.com>
2023-05-16 22:24:32 -0300

    Doing some logic

--- cashRegister.js
+++ cashRegister.js
@@ -17,25 +17,41 @@ const currencyValues = {
 }
 
 function checkCashRegister(price, cash, cid) {
-    if()
 	let change = cash - price
+	let totalCashInDrawer = 0
 
 	let drawer = cid.map((item) => {
+		totalCashInDrawer += Math.ceil(item[1])
 		return [item[0], Math.ceil(item[1] / currencyValues[item[0]])]
 	})
 
-	console.log(change)
-	console.log(drawer)
+	console.log('Cash in draw: ' + totalCashInDrawer)
+	console.log('Change: ' + change)
+
+	// falta a parte de se nao tiver o troco exato
+	if (totalCashInDrawer < change) {
+		return { status: 'INSUFFICIENT_FUNDS', change: [] }
+	}
+	// Caso o valor de troco seja o mesmo do que tem no caixa
+	else if (totalCashInDrawer === change) {
+		return { status: 'CLOSED', change: cid }
+	}
+	// Caso tenha troco suficiente
+	else if (totalCashInDrawer > change) {
+		return { status: 'OPEN', change: [] }
+	}
 }
 
-checkCashRegister(19.5, 20, [
-	['PENNY', 1.01],
-	['NICKEL', 2.05],
-	['DIME', 3.1],
-	['QUARTER', 4.25],
-	['ONE', 90],
-	['FIVE', 55],
-	['TEN', 20],
-	['TWENTY', 60],
-	['HUNDRED', 100],
-])
+console.log(
+	checkCashRegister(19.5, 20, [
+		['PENNY', 1.01],
+		['NICKEL', 2.05],
+		['DIME', 3.1],
+		['QUARTER', 4.25],
+		['ONE', 90],
+		['FIVE', 55],
+		['TEN', 20],
+		['TWENTY', 60],
+		['HUNDRED', 100],
+	])
+)
